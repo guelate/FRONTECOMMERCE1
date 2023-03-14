d78ff2e74214c6b096bc9089237bc90dfcd15b6f
kseyo <kseyo@veepee.com>
2023-03-14 16:44:19 +0100

    progression

--- note.md
+++ note.md
@@ -1,7 +1,7 @@
 display:block == set a block element in full width line 
 
 review: naming class/variable
-stop at HEADER
+stop at HEADER show / start of //base -img logic css
 
 
 
@@ -9,4 +9,4 @@ step:
 
 1 draft of each area (base, class , layout ect..)
 2 create variable 
-
+3 apply default properties on differents balises and elements
--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 //MENU SHOW
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
 
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('nav-toggle','nav-menu')
 
 //REMOVE MENU 
+const navLink = document.querySelectorAll('.nav__link'),
+    navMenu = document.getElementById('nav-menu')
 
+    //difference funct et const ()
+function linkAction(){
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 //SCROLL SECTIONS ACTIVE LINK 
 
