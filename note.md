982753deaca8351f4dbf3923568785112a8533ab
kseyo <kseyo@veepee.com>
2023-03-15 16:06:36 +0100

    home sneaker button

--- note.md
+++ note.md
@@ -3,6 +3,8 @@ display:block == set a block element in full width line
 review: naming class/variable
 stop at HEADER show / start of //base -img logic css
 
+https://www.volusion.com/blog/css-selectors/#:~:text=Attribute%20selectors%20are%20used%20to,are%20denoted%20with%20square%20brackets. (bracket in css)
+
 
 
 step:
@@ -10,3 +12,7 @@ step:
 1 draft of each area (base, class , layout ect..)
 2 create variable 
 3 apply default properties on differents balises and elements
+
+sujet interessting
+
+shortcut code like: w220px -> width:220px;
--- script.js
+++ script.js
@@ -1,48 +1,57 @@
 //MENU SHOW
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId);
-    const nav = document.getElementById(navId); 
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
-        })
-    }
-}
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
 
-showMenu('nav-toggle','nav-menu')
+showMenu("nav-toggle", "nav-menu");
 
-//REMOVE MENU 
-const navLink = document.querySelectorAll('.nav__link');
-const navMenu = document.getElementById('nav-menu');
+//REMOVE MENU
+const navLink = document.querySelectorAll(".nav__link");
+const navMenu = document.getElementById("nav-menu");
 
-    //difference funct et const ()
-function linkAction(){
-    navMenu.classList.remove('show')
+//difference funct et const ()
+function linkAction() {
+  navMenu.classList.remove("show");
 }
 
 //difference between '' and ""
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-//SCROLL SECTIONS ACTIVE LINK 
-const sections = document.querySelectorAll('selection[id]');
-window.addEventListener('scroll',scrollActive)
+//SCROLL SECTIONS ACTIVE LINK
+const sections = document.querySelectorAll("section[id]");
+window.addEventListener("scroll", scrollActive);
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-    sections.forEach(current =>{
-        const sectionHeight= current.offsetHeight
-        const sectionTop = current.offsetTop - 50
-        sectionId = curent.getAttribute('id')
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute("id");
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.add('active')
-        }else{
-            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.add('remove')
-
-        }
-    })
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("remove");
+    }
+  });
 }
 
 //CHANGE COLOR HEADER
+//simplifly this part
+window.onscroll = () => {
+  const nav = document.getElementById("header");
+  if (this.scroll >= 200) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+};
