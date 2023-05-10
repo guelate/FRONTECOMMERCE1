c444bf6b9fd9fb04dbd0ef0402db543d6c761966
kseyo <kseyo@veepee.com>
2023-05-10 11:18:11 +0200

    stop tailwind css

--- startshop/src/components/Newsletter.js
+++ startshop/src/components/Newsletter.js
@@ -1,23 +1,7 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import { Newsletter_STYLE_DIV_1, Newsletter_STYLE_FORM, Newsletter_STYLE_H3, Newsletter_STYLE_P } from '../utils/Constants/style';
-import './full.css'
-
-const Newsletter = ({h3,p,button}) => {
-    return(
-        
-        <div class={Newsletter_STYLE_DIV_1}>
-          <div>
-            <h3 class={Newsletter_STYLE_H3}>{h3}</h3>
-            <p class={Newsletter_STYLE_P}>{p}</p>
-          </div>
-            <form action="" class={Newsletter_STYLE_FORM}>
-            <input name="myInput" />
-
-              {/* <a href="#" class="button">{button}</a> IMPORTATION BUTTON*/}
-            </form>
-          </div>
-        
-    );
-
+const Newsletter = () => {
+  return(
+    <>
+    </>
+  )
 }
 export default Newsletter
--- startshop/src/components/SneakerSaleCard.js
+++ startshop/src/components/SneakerSaleCard.js
@@ -1,29 +1,8 @@
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
-import "./style.css";
-import hightNike1 from "../assets/img/hightNike1.png";
-import {
-  SneakerSaleCard_STYLE_ARTICLE,
-  SneakerSaleCard_STYLE_DIV_1,
-  SneakerSaleCard_STYLE_IMG,
-} from "../utils/Constants/style";
-
-const SneakerSaleCard = ({ vente, model, price, misc }) => {
-  return (
-    <article class={SneakerSaleCard_STYLE_ARTICLE}>
-      <div class={SneakerSaleCard_STYLE_DIV_1}>{vente}</div>
-      <img src={hightNike1} class={SneakerSaleCard_STYLE_IMG} />
-      <span class="sneaker__name">{model}</span>
-      <span class="sneaker__preci">{price}</span>
-      <a href="" class="button__light">
-        {misc}
-      </a>
-    </article>
-  );
-};
-
-export default SneakerSaleCard;
-
-//reprendre l'animation des cartes nouveautés & ne pas faire la partie nouveauté
-//constante pour les couleurs et le style
+const SneakerSaleCard = () => {
+  return(
+    <>
+    
+    </>
+  )
+}
+export default SneakerSaleCard
